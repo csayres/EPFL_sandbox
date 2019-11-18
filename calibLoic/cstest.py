619a25d5849cb1e5b597d4a90082b00bcef9d27a
Conor Sayres <csayres@uw.edu>
2019-11-18 14:01:18 +0100

    a

--- calibLoic/cstest.py
+++ calibLoic/cstest.py
@@ -1,5 +1,9 @@
 import classTestBench as tb
+import DEFINES
+import classCamera as cam
 
 if __name__ == "__main__":
-    bench = tb.TestBench()
+    camCfgFile = "Config/Cameras/camera_22289804.mat" # this lives on tendo only, it's large
+    camera = cam.Camera(DEFINES.PC_CAMERA_TYPE_XY)
+    camera.setDistortionCorrection(camCfgFile)
     import pdb; pdb.set_trace()
