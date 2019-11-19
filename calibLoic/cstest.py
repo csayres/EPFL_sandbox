1ec5e57c7eebd96f593773cd1b52feb7076b4039
Conor Sayres <csayres@uw.edu>
2019-11-19 09:52:50 +0100

    a

--- calibLoic/cstest.py
+++ calibLoic/cstest.py
@@ -1,23 +1,89 @@
 # import classTestBench as tb
-import DEFINES
-import classCamera as cam
 import matplotlib.image as mpimg
-import classConfig
+import matplotlib.pyplot as plt
+from astropy.io import fits
+from scipy import io
+import numpy
 
 exposure_time = 4748.3 # says the optimal exposure time calculator?
 gain = 0.0
 black_level = 1.25
 gamma = 1.0
-filename = "outfile.tiff"
+baseImg = "img/img"
+fileCounter = 0
+# camCfgFile = "Config/Cameras/camera_22289804.mat"
 
-if __name__ == "__main__":
+##### camera distortion stuff ####################
+# import DEFINES
+# import classConfig
+# import classCamera as cam
+# cam_distortion = io.loadmat("Config/Cameras/camera_22289804.mat")
+# cam_distortion = cam_distortion[DEFINES.PC_FILE_DISTORTION_PARAMETERS_NAME]
+# cam_x_corr = cam_distortion[DEFINES.PC_FILE_DISTORTION_XCORR_NAME]
+# cam_y_corr = cam_distortion[DEFINES.PC_FILE_DISTORTION_YCORR_NAME]
+# cam_scale_factor = cam_distortion[DEFINES.PC_FILE_DISTORTION_SCALE_FACTOR_NAME]
+# xCorr = numpy.nan_to_num(cam_x_corr[0,0])
+# yCorr = numpy.nan_to_num(cam_y_corr[0,0])
+# scaleFactor = cam_scale_factor[0,0][0,0]
+
+######### camera controller #########################
+# camera = cam.Camera(cameraType = DEFINES.PC_CAMERA_TYPE_XY, compatibleCameraID=22289804) #22942361 #22994237
+# camera.setMaxROI()
+# config = classConfig.Config()
+# camera.setDistortionCorrection(config)
+# camera.setProperties(exposure_time, gain, black_level, gamma)
+
+########## PyGuide #####################################
+
+
+def expose():
     # camCfgFile = "Config/Cameras/camera_22289804.mat" # this lives on tendo only, it's large
+    import DEFINES
+    import classConfig
+    import classCamera as cam
+    cam_distortion = io.loadmat("Config/Cameras/camera_22289804.mat")
+    cam_distortion = cam_distortion[DEFINES.PC_FILE_DISTORTION_PARAMETERS_NAME]
+    cam_x_corr = cam_distortion[DEFINES.PC_FILE_DISTORTION_XCORR_NAME]
+    cam_y_corr = cam_distortion[DEFINES.PC_FILE_DISTORTION_YCORR_NAME]
+    cam_scale_factor = cam_distortion[DEFINES.PC_FILE_DISTORTION_SCALE_FACTOR_NAME]
+    xCorr = numpy.nan_to_num(cam_x_corr[0,0])
+    yCorr = numpy.nan_to_num(cam_y_corr[0,0])
+    scaleFactor = cam_scale_factor[0,0][0,0]
     camera = cam.Camera(cameraType = DEFINES.PC_CAMERA_TYPE_XY, compatibleCameraID=22289804) #22942361 #22994237
     camera.setMaxROI()
     config = classConfig.Config()
     camera.setDistortionCorrection(config)
-    # camera.setDistortionCorrection(camCfgFile)
     camera.setProperties(exposure_time, gain, black_level, gamma)
-    picture = camera.getImage()
-    mpimg.imsave(filename,picture)
-    # import pdb; pdb.set_trace()
+    imgData = camera.getImage()
+    print("imgData shape", imgData.shape)
+    # mpimg.imsave(filename,picture)
+
+def findCentroids(imgData):
+    import PyGuide
+    CCDInfo = PyGuide.CCDInfo(
+        bias = 2,    # image bias, in ADU
+        readNoise = 2, # read noise, in e-
+        ccdGain = 2,  # inverse ccd gain, in e-/ADU
+    )
+    # 1598, 1313 measured in ds9 from 0,0 at bottom left
+    # img = mpimg.imread("outfile.tiff")
+    # print(img.shape)
+    # img = img[::-1,:, 0]
+    # plt.imshow(img, origin="lower")
+    # plt.show()
+    mask=None
+    ctrDataList, imStats = PyGuide.findStars(
+        data = imgData,
+        mask = mask,
+        satMask = None,
+        ccdInfo = CCDInfo,
+        verbosity = 0,
+        doDS9 = False,
+    )[0:2]
+    for ctrData in ctrDataList:
+        xyCtr = ctrData.xyCtr
+        rad = ctrData.rad
+        print("star xyCtr=%.2f, %.2f, radius=%s" % (xyCtr[0], xyCtr[1], rad))
+
+if __name__ == "__main__":
+    expose()
