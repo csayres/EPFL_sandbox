aeb428282842817f0799ba10f18fae032ab607df
Conor Sayres <csayres@uw.edu>
2019-11-20 15:13:28 +0100

    a

--- moveSevenCamera.py
+++ moveSevenCamera.py
@@ -203,6 +203,7 @@ def centroid(imgData, xyKaijuMM):
     mask[startCol:endCol, startRow:endRow] = 200
 
     plt.imshow(imgData + mask)
+    plt.imshow(mask)
     plt.show()
     # plt.savefig("test.tiff")
     # plt.close()
